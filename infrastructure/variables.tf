b7ca94f8abdd9ad9b2bd3f57e6a63a38dafb86d0
Rooarii <rooarii.manuel@gmail.com>
2022-05-27 16:21:51 +0200

    Create tf variables
    Signed-off-by: Rooarii <rooarii.manuel@gmail.com>

---
+++ infrastructure/variables.tf
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These parameters must be supplied.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "project_id" {
+  description = "The id of the GCP projetc"
+  type = string
+}
+
+variable "credentials_file" {
+  description = "This is the path to the credentials file used to authenticate to GCP"
+  type = string
+}

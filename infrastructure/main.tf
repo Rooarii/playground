30dae831783194bb818a305e363532452dfed88e
Rooarii <rooarii.manuel@gmail.com>
2022-06-29 23:35:42 +0200

    Infra: Define permissions for Cloud Run service
    Signed-off-by: Rooarii <rooarii.manuel@gmail.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -216,3 +216,24 @@ resource "google_cloud_run_service" "service" {
     latest_revision = true
   }
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# IAM
+# Define cloud run permissions
+# ---------------------------------------------------------------------------------------------------------------------
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.service.location
+  project  = google_cloud_run_service.service.project
+  service  = google_cloud_run_service.service.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}

8ffc17d9506d2310e0178793671e5aec33636f44
Rooarii <rooarii.manuel@gmail.com>
2022-06-29 22:48:04 +0200

    Infra: Create PostgreSQL database resource
    Signed-off-by: Rooarii <rooarii.manuel@gmail.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -67,3 +67,32 @@ resource "google_project_service" "secretmanager" {
 resource "google_service_account" "django" {
   account_id = "django"
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DATABASE CREATION
+# --------------------------------------------------------------------------------------------------------------------
+resource "random_password" "database_password" {
+  length  = 32
+  special = false
+}
+
+resource "google_sql_database_instance" "instance" {
+  name             = "personnal_website"
+  database_version = "POSTGRES_14"
+  region           = "eu-central1"
+  settings {
+    tier = "db-f1-micro"
+  }
+  deletion_protection = true
+}
+
+resource "google_sql_database" "database" {
+  name     = "django"
+  instance = google_sql_database_instance.instance.name
+}
+
+resource "google_sql_user" "django" {
+  name     = "django"
+  instance = google_sql_database_instance.instance.name
+  password = random_password.database_password.result
+}

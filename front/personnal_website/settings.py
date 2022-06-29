e3d5fe9dd1c58a2472e155b29a338960c4ed7a99
Rooarii <rooarii.manuel@gmail.com>
2022-06-29 21:02:16 +0200

    DB: Create specific profile for postgreSQL local db
    this profile shall be used for development purpose only. User, User Password have been arbitrarily choosen
    
    Signed-off-by: Rooarii <rooarii.manuel@gmail.com>

--- front/personnal_website/settings.py
+++ front/personnal_website/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-gk!jk@!b$4xjm0^zz9lzg^+a6nq=*$fu_75fl=9wm99osq1y!&
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,17 +67,35 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'personnal_website.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # TODO: Change db-kind and setup connection
+    # Profile for production purpose
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    # Profile for development purpose.
+
+    # NOTE: to use local_db, follow the steps below:
+    # - install postgresql locally
+    # - log in to postgresql
+    # - create the following user with password and authorizations
+    # - make migrations
+    # - migrate
+    # - runserver
+    # 🎉 local postgresql db up, running & connected to django webapp.
+    'local': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'local_db',
+        'USER': 'dev',
+        'PASSWORD': 'davethedev',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -99,7 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -110,7 +125,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
